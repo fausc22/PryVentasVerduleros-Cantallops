fc68fb12fe977548b44e098fd4c5988205fddadc
root <mgiraudo@ies21.edu.ar>
2023-06-07 13:45:31 -0300

    Agregar archivos de proyecto.

---
+++ frmVentas.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace PryVentasVerduleros_Cantallops
+{
+    partial class frmVentas
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.mrcVenta = new System.Windows.Forms.GroupBox();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.cmbProducto = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbVendedor = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mrcVenta.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(52, 9);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(199, 31);
+            this.lblTitulo.TabIndex = 1;
+            this.lblTitulo.Text = "VERDULERIA";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 172);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // mrcVenta
+            // 
+            this.mrcVenta.Controls.Add(this.btnBorrar);
+            this.mrcVenta.Controls.Add(this.btnRegistrar);
+            this.mrcVenta.Controls.Add(this.cmbProducto);
+            this.mrcVenta.Controls.Add(this.textBox1);
+            this.mrcVenta.Controls.Add(this.label3);
+            this.mrcVenta.Controls.Add(this.cmbVendedor);
+            this.mrcVenta.Controls.Add(this.label2);
+            this.mrcVenta.Controls.Add(this.label1);
+            this.mrcVenta.Location = new System.Drawing.Point(12, 63);
+            this.mrcVenta.Name = "mrcVenta";
+            this.mrcVenta.Size = new System.Drawing.Size(272, 223);
+            this.mrcVenta.TabIndex = 7;
+            this.mrcVenta.TabStop = false;
+            this.mrcVenta.Text = "NUEVA VENTA";
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnBorrar.Location = new System.Drawing.Point(161, 125);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(88, 26);
+            this.btnBorrar.TabIndex = 11;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = false;
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnRegistrar.Location = new System.Drawing.Point(161, 81);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(88, 26);
+            this.btnRegistrar.TabIndex = 10;
+            this.btnRegistrar.Text = "Registrar Venta";
+            this.btnRegistrar.UseVisualStyleBackColor = false;
+            // 
+            // cmbProducto
+            // 
+            this.cmbProducto.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbProducto.FormattingEnabled = true;
+            this.cmbProducto.Location = new System.Drawing.Point(9, 110);
+            this.cmbProducto.Name = "cmbProducto";
+            this.cmbProducto.Size = new System.Drawing.Size(121, 21);
+            this.cmbProducto.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Producto";
+            // 
+            // cmbVendedor
+            // 
+            this.cmbVendedor.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbVendedor.FormattingEnabled = true;
+            this.cmbVendedor.Location = new System.Drawing.Point(9, 59);
+            this.cmbVendedor.Name = "cmbVendedor";
+            this.cmbVendedor.Size = new System.Drawing.Size(121, 21);
+            this.cmbVendedor.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cantidad (kg)";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Vendedor";
+            // 
+            // frmVentas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(307, 312);
+            this.Controls.Add(this.mrcVenta);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "frmVentas";
+            this.Text = "VENTAS";
+            this.Load += new System.EventHandler(this.frmVentas_Load);
+            this.mrcVenta.ResumeLayout(false);
+            this.mrcVenta.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox mrcVenta;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.ComboBox cmbProducto;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbVendedor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ frmVentas.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.OleDb;
+
+namespace PryVentasVerduleros_Cantallops
+{
+    public partial class frmVentas : Form
+    {
+        
+        public frmVentas()
+        {
+            InitializeComponent();
+        }
+
+        private void btnObtener_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void frmVentas_Load(object sender, EventArgs e)
+        {
+            ClsConexion objCnn = new ClsConexion();
+            objCnn.CargarDatos(cmbProducto, cmbVendedor);
+        }
+    }
+}
